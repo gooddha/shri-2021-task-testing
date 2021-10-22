d10aeb805342096a03b03ba2bb1a832c84f9a811
Andrey Adamovich <gooddha@ya.ru>
2021-10-22 12:46:57 +0300

    add test for checking product properties

--- test/unit/data.test.ts
+++ test/unit/data.test.ts
@@ -14,7 +14,7 @@ describe('Should return products list', () => {
   });
 });
 
-describe('Should return product by id', () => {
+describe('Should return product by ID', () => {
   const product = store.getProductById(0);
 
   it('Should be instance of Object', () => {
@@ -23,6 +23,41 @@ describe('Should return product by id', () => {
 
   it('Should has id property', () => {
     expect(product).toHaveProperty('id', 0);
+  });
+
+  it('Should has name, description, price, color, material properties', () => {
+    expect(product).toHaveProperty('name');
+    expect(product).toHaveProperty('description');
+    expect(product).toHaveProperty('price',);
+    expect(product).toHaveProperty('color');
+    expect(product).toHaveProperty('material');
+  });
+
+});
+
+describe('Should create order', () => {
+  const order = {
+    form: {
+      name: 'Test',
+      phone: '+77777777',
+      address: 'Moscow',
+    },
+    cart: {}
+  };
+
+  const createOrder = store.createOrder(order);
+
+  it('Should return ID with type number', () => {
+    expect(typeof createOrder).toEqual('number');
+  });
+
+});
+
+describe('Should return orders array', () => {
+  const orders = store.getLatestOrders();
+
+  it('Should be instance of Array', () => {
+    expect(orders).toBeInstanceOf(Array);
   });
 
 });
