224527dcc440548fdaf0472bf4fb9ff770ea1f4a
Andrey Adamovich <gooddha@ya.ru>
2021-10-29 16:42:03 +0300

    add test for product details page

--- test/unit/catalog.test.tsx
+++ test/unit/catalog.test.tsx
@@ -11,44 +11,55 @@ import { it, expect } from '@jest/globals';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import events from '@testing-library/user-event';
 
-import { ProductShortInfo } from '../../src/common/types';
+import { ProductShortInfo, Product } from '../../src/common/types';
 import { initStore } from '../../src/client/store';
 import { ExampleApi, CartApi } from '../../src/client/api';
 import { Application } from '../../src/client/Application';
-import { ProductItem } from '../../src/client/components/ProductItem';
-import { ApplicationState } from '../../src/client/store';
 
 
 describe('Проверка страницы Catalog', () => {
-  describe('Проверка отображения списка товаров', () => {
-    const basename = '/hw/store';
-    const api = new ExampleApi(basename);
+  const basename = '/hw/store';
+  const api = new ExampleApi(basename);
 
-    api.getProducts = async () => {
-      //@ts-ignore
-      return await Promise.resolve<AxiosResponse<ProductShortInfo[], any>>({
-        data: [
-          {
-            id: 1,
-            name: `Test product 1`,
-            price: Number(1)
-          },
-          {
-            id: 2,
-            name: `Test product 2`,
-            price: Number(2)
-          }
-        ]
-      });
-    }
-
-    const cart = new CartApi();
-    const store = initStore(api, cart);
-    const history = createMemoryHistory({
-      initialEntries: ['/catalog'],
-      initialIndex: 0
+  api.getProducts = async () => {
+    //@ts-ignore
+    return await Promise.resolve<AxiosResponse<ProductShortInfo[], any>>({
+      data: [
+        {
+          id: 1,
+          name: `Test product 1`,
+          price: Number(1)
+        },
+        {
+          id: 2,
+          name: `Test product 2`,
+          price: Number(2)
+        }
+      ]
     });
+  }
 
+  api.getProductById = async (id: number) => {
+    return await Promise.resolve({
+      data: {
+        id,
+        name: `Test product`,
+        description: 'Test description',
+        price: Number(1),
+        color: 'Test color',
+        material: 'Test material',
+      }
+    });
+  }
+
+  const cart = new CartApi();
+  const store = initStore(api, cart);
+  const history = createMemoryHistory({
+    initialEntries: ['/catalog'],
+    initialIndex: 0
+  });
+
+  describe('Проверка отображения списка товаров', () => {
     it('В каталоге должны отображаться товары, список которых приходит с сервера', async () => {
       const application = (
         <Router history={history}>
@@ -67,8 +78,6 @@ describe('Проверка страницы Catalog', () => {
 
       expect(filtered).toHaveLength(2)
     });
-
-
 
     it('Для каждого товара в каталоге отображается название, цена и ссылка на страницу с подробной информацией о товаре', async () => {
 
@@ -94,6 +103,52 @@ describe('Проверка страницы Catalog', () => {
       expect(itemPrice).toHaveTextContent('$1');
       expect(itemLink).toHaveAttribute('href', '/catalog/1');
     });
+
   });
+
+  describe('Проверка страницы с подробной информацией о товаре', () => {
+
+    it('На странице отображаются: название товара, его описание, цена, цвет, материал и кнопка "добавить в корзину"', async () => {
+      //@ts-ignore
+      const application = (
+        <Router history={history}>
+          <Provider store={store}>
+            <Application />
+          </Provider>
+        </Router>
+      );
+
+      const { findAllByTestId, getByText, getByRole } = render(application);
+      await waitForElementToBeRemoved(() => getByText(/loading/i));
+
+      const items = await findAllByTestId('1');
+      const itemBody = items.filter(item => item.classList.contains('ProductItem'))[0];
+      const itemLink = [...itemBody.getElementsByClassName('ProductItem-DetailsLink')][0];
+      events.click(itemLink);
+      await waitForElementToBeRemoved(() => getByText(/loading/i));
+
+      const itemName = getByRole('heading', { name: /test product/i });
+      const itemDescription = itemName.nextElementSibling;
+      const itemPrice = itemDescription.nextElementSibling;
+      const button = getByRole('button', { name: /add to cart/i });
+      const color = getByText(/Color/).nextElementSibling;
+      const material = getByText(/Material/).nextElementSibling;
+
+
+      expect(itemName).toHaveTextContent('Test product');
+      expect(itemDescription).toHaveTextContent('Test description');
+      expect(itemPrice).toHaveTextContent('$1');
+      expect(button).toBeInTheDocument();
+      expect(button).toHaveTextContent('Add to Cart');
+      expect(color).toHaveTextContent('Test color');
+      expect(material).toHaveTextContent('Test material');
+
+
+
+      screen.logTestingPlaygroundURL();
+    });
+
+  });
+
 
 });
